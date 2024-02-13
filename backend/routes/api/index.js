2060a8e97bafe686136de266ad9492404f80d6cf
Max Fonda <mxwll155@gmail.com>
2024-02-12 20:55:03 -0500

    Add User authentication Express middleware

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,13 +1,6 @@
-const router = require('express').Router();
-
-
-
-router.post('/test', function(req, res) {
-    res.json({ requestBody: req.body });
-  });
-
-
-
-
+const router = require("express").Router();
+const { User } = require("../../db/models");
+const { restoreUser } = require("../../utils/auth.js");
+router.use(restoreUser);
 
 module.exports = router;
