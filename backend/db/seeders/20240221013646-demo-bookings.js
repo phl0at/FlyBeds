3f1db7bec92e97ecbfd1cf99262553b631825789
Max Fonda <mxwll155@gmail.com>
2024-02-21 15:05:50 -0500

    added Bookings seed data

--- backend/db/seeders/20240221013646-demo-bookings.js
+++ backend/db/seeders/20240221013646-demo-bookings.js
@@ -1,25 +1,48 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
+"use strict";
+const { Booking } = require("../models");
+let options = {};
+if (process.env.NODE_ENV === "production") {
+  options.schema = process.env.SCHEMA; // define your schema in options object
+}
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    await Booking.bulkCreate([
+      {
+        userId: 3,
+        spotId: 1,
+        startDate: "01-01-2024",
+        endDate: "01-04-2024",
+      },
+      {
+        userId: 2,
+        spotId: 1,
+        startDate: "01-10-2024",
+        endDate: "01-14-2024",
+      },
+      {
+        userId: 2,
+        spotId: 1,
+        startDate: "02-01-2024",
+        endDate: "02-04-2024",
+      },
+      {
+        userId: 1,
+        spotId: 3,
+        startDate: "02-05-2024",
+        endDate: "02-10-2024",
+      },
+    ]);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    options.tableName = "Bookings";
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        userId: { [Op.in]: [1, 2, 3, 4] },
+      },
+      {}
+    );
+  },
 };
