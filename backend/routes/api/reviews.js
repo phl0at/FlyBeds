6f1ae12cb700f6ccc44181051696569ad61d1379
Max Fonda <mxwll155@gmail.com>
2024-02-22 20:07:16 -0500

    Third reviews route complete

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -11,7 +11,7 @@ const { Review } = require("../../db/models");
 const { ReviewImage } = require("../../db/models");
 const router = express.Router();
 
-// ------ GET REVIEWS OF CURRENT USER ------
+// ------ GET REVIEWS OF CURRENT USER ------ //
 
 router.get("/current", requireAuth, async (req, res) => {
   const currUser = req.user.dataValues;
@@ -22,7 +22,7 @@ router.get("/current", requireAuth, async (req, res) => {
     include: [
       {
         model: User,
-        attributes: ["id", "firstName", "lastName"]
+        attributes: ["id", "firstName", "lastName"],
       },
       {
         model: Spot,
@@ -39,9 +39,44 @@ router.get("/current", requireAuth, async (req, res) => {
     ],
   });
 
-
-
   return res.json({ Reviews: reviewData });
+});
+
+// ------ ADD IMAGE TO REVIEW BY REVIEW ID ------ //
+
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+  let { reviewId } = req.params;
+  reviewId = Number(reviewId);
+  const currUser = req.user.dataValues;
+  const { url } = req.body;
+  const reviewData = await Review.findByPk(reviewId);
+  if (!reviewData)
+    return res.status(404).json({ message: "Review couldn't be found" });
+
+  const allImages = await ReviewImage.findAll({
+    where: {
+      reviewId,
+    },
+  });
+
+  if (allImages.length >= 10)
+    return res
+      .status(403)
+      .json({
+        message: "Maximum number of images for this resource was reached",
+      });
+
+  const newImage = await ReviewImage.create({
+    reviewId,
+    url,
+  });
+  const imageData = await ReviewImage.findOne({
+    where: {
+      id: newImage.id
+    },
+    attributes: ["id", "url"],
+  });
+  return res.json(imageData);
 });
 
 module.exports = router;
