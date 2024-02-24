828451cd42533b42976b8336d80dc4a2e66aff7f
Max Fonda <mxwll155@gmail.com>
2024-02-24 15:50:27 -0500

    user routes all functioning as intended

--- backend/app.js
+++ backend/app.js
@@ -62,7 +62,7 @@ app.use((err, _req, _res, next) => {
 app.use((err, _req, res, _next) => {
   res.status(err.status || 500);
   console.error(err);
-  if (!isProduction) {
+  if (isProduction) {
     if (err.message === "Authentication required" || err.message === "Invalid credentials") {
       return res.json({ message: err.message });
     }
