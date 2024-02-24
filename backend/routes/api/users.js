9090d1512a21c0419e3fe248ea6f4dc850fda3e9
Max Fonda <mxwll155@gmail.com>
2024-02-24 13:51:25 -0500

    fixed all user validation errors (wrong branch sorry)

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -11,12 +11,20 @@ const validateSignup = [
   check("email")
     .exists({ checkFalsy: true })
     .isEmail()
-    .withMessage("Please provide a valid email."),
+    .withMessage("Invalid email"),
   check("username")
     .exists({ checkFalsy: true })
     .isLength({ min: 4 })
-    .withMessage("Please provide a username with at least 4 characters."),
+    .withMessage("Username is required"),
   check("username").not().isEmail().withMessage("Username cannot be an email."),
+  check("firstName")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("First Name is required"),
+  check("lastName")
+    .exists({ checkFalsy: true })
+    .notEmpty()
+    .withMessage("Last Name is required"),
   check("password")
     .exists({ checkFalsy: true })
     .isLength({ min: 6 })
