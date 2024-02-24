2731bc5ffbb9faf7129f0947d9b9e1384b6afb40
Max Fonda <mxwll155@gmail.com>
2024-02-24 14:54:09 -0500

    bookings route file setup

---
+++ backend/routes/api/bookings.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const sequelize = require("sequelize");
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
+const { Spot } = require("../../db/models");
+const { SpotImage } = require("../../db/models");
+const { Review } = require("../../db/models");
+const { ReviewImage } = require("../../db/models");
+const { Booking } = require("../../db/models")
+
+const router = express.Router();
+
+router.get("/current", requireAuth, async (req, res) => {
+
+})
+
+
+
+
+
+
+
+
+module.exports = router;
