dd8e606aca78416f496280ac70bd21857ecda22a
Max Fonda <mxwll155@gmail.com>
2024-04-04 18:59:17 -0400

    front-end token routes set up

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { Provider } from 'react-redux';
-import App from './App';
-import './index.css';
-import configureStore from './store';
-
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
+import configureStore from "./store";
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 const store = configureStore();
 
-if (process.env.NODE_ENV !== 'production') {
+if (process.env.NODE_ENV !== "production") {
   window.store = store;
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+if (import.meta.env.MODE !== "production") {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
