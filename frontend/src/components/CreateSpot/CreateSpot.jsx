f6a7992869fe6ba1810af185f2a7e102562d7ad2
Max Fonda <mxwll155@gmail.com>
2024-04-14 17:42:01 -0400

    cleaned up WETness of spot form

--- frontend/src/components/CreateSpot/CreateSpot.jsx
+++ frontend/src/components/CreateSpot/CreateSpot.jsx
@@ -43,6 +43,20 @@ const CreateSpot = () => {
     setErrors({});
   }, []);
 
+  const textInput = (value, placeholder, setter) => {
+    return (
+      <input
+        type="text"
+        value={value}
+        name={placeholder}
+        placeholder={placeholder}
+        onChange={(e) => {
+          setter(e.target.value);
+        }}
+      />
+    );
+  };
+
   const onSubmit = async (e) => {
     e.preventDefault();
 
@@ -107,85 +121,74 @@ const CreateSpot = () => {
       </h5>
       <form onSubmit={onSubmit}>
         <div className="spot-location">
-          <label htmlFor="country">Country</label>
+          {/*
+      //!-------------------------------------------------------------------
+      //?---------------------- LOCATION INPUTS ----------------------------
+      //!-------------------------------------------------------------------
+      */}
+          <label htmlFor="Country">Country</label>
           {errors.country && (
             <div color="red" className="errors">
               {errors.country}
             </div>
           )}
-          <input
-            name="country"
-            type="text"
-            value={country}
-            placeholder="Country"
-            onChange={(e) => setCountry(e.target.value)}
-          />
-          <label htmlFor="street">Street Address</label>
+          {textInput(country, "Country", setCountry)}
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          <label htmlFor="Address">Street Address</label>
           {errors.street && (
             <div color="red" className="errors">
               {errors.street}
             </div>
           )}
-          <input
-            name="street"
-            type="text"
-            value={address}
-            placeholder="Street Address"
-            onChange={(e) => setAddress(e.target.value)}
-          />
-          <label htmlFor="city">City</label>
+          {textInput(address, "Address", setAddress)}
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          <label htmlFor="City">City</label>
           {errors.city && (
             <div color="red" className="errors">
               {errors.city}
             </div>
           )}
-          <input
-            name="city"
-            type="text"
-            value={city}
-            placeholder="City"
-            onChange={(e) => setCity(e.target.value)}
-          />
-          <label htmlFor="state">State</label>
+          {textInput(city, "City", setCity)}
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          <label htmlFor="State">State</label>
           {errors.state && (
             <div color="red" className="errors">
               {errors.state}
             </div>
           )}
-          <input
-            name="state"
-            type="text"
-            value={state}
-            placeholder="State"
-            onChange={(e) => setState(e.target.value)}
-          />
-          <label htmlFor="lat">Latitude</label>
+          {textInput(state, "State", setState)}
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          <label htmlFor="Latitude">Latitude</label>
           {errors.lat && (
             <div color="red" className="errors">
               {errors.lat}
             </div>
           )}
-          <input
-            name="lat"
-            type="text"
-            value={lat}
-            placeholder="Latitude"
-            onChange={(e) => setLat(e.target.value)}
-          />
-          <label htmlFor="lng">Longitude</label>
+          {textInput(lat, "Latitude", setLat)}
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          <label htmlFor="Longitude">Longitude</label>
           {errors.lng && (
             <div color="red" className="errors">
               {errors.lng}
             </div>
           )}
-          <input
-            name="lng"
-            type="text"
-            value={lng}
-            placeholder="Longitude"
-            onChange={(e) => setLng(e.target.value)}
-          />
+          {textInput(lng, "Longitude", setLng)}
         </div>
+        {/*
+        //!-------------------------------------------------------------------
+        //?------------------------ DESCRIPTION ------------------------------
+        //!-------------------------------------------------------------------
+        */}
         <div className="spot-description">
           <label htmlFor="description">Describe your place to guests</label>
           <h5>
@@ -205,114 +208,103 @@ const CreateSpot = () => {
             </div>
           )}
         </div>
+        {/*
+        //!-------------------------------------------------------------------
+        //?----------------------- NAME/TITLE --------------------------------
+        //!-------------------------------------------------------------------
+        */}
         <div className="spot-name">
-          <label htmlFor="name">Create a title for your spot</label>
+          <label htmlFor="Name your Spot">Create a title for your spot</label>
           <h5>
             {`Catch guests' attention with a spot title that highlights what makes
             your place special.`}
           </h5>
-          <input
-            name="name"
-            type="text"
-            value={name}
-            placeholder="Name your spot"
-            onChange={(e) => setName(e.target.value)}
-          />
+          {textInput(name, "Name your Spot", setName)}
           {errors.name && (
             <div color="red" className="errors">
               {errors.name}
             </div>
           )}
         </div>
+        {/*
+        //!-------------------------------------------------------------------
+        //?--------------------------- PRICE ---------------------------------
+        //!-------------------------------------------------------------------
+        */}
         <div className="spot-price">
-          <label htmlFor="price">Set a base price for your spot</label>
+          <label htmlFor="Price per night (USD)">
+            Set a base price for your spot
+          </label>
           <h5>
             Competitive pricing can help your listing stand out and rank higher
             in search results.
           </h5>
           <div id="$">$</div>
-          <input
-            name="price"
-            type="text"
-            value={price}
-            placeholder="Price per night (USD)"
-            onChange={(e) => setPrice(e.target.value)}
-          />
+          {textInput(price, "Price per night (USD)", setPrice)}
           {errors.price && (
             <div color="red" className="errors">
               {errors.price}
             </div>
           )}
         </div>
+        {/*
+        //!-------------------------------------------------------------------
+        //?-------------------------- IMAGES ---------------------------------
+        //!-------------------------------------------------------------------
+        */}
         <div className="spot-images">
-          <label htmlFor="preview-image">Liven up your spot with photos</label>
+          <label htmlFor="Preview Image URL">
+            Liven up your spot with photos
+          </label>
           <h5>Submit a link to at least one photo to publish your spot.</h5>
-          <input
-            className="preview-image"
-            name="preview-image"
-            type="text"
-            value={previewImage}
-            placeholder="Preview Image URL"
-            onChange={(e) => setPreviewImage(e.target.value)}
-          />
+          {textInput(previewImage, "Preview Image URL", setPreviewImage)}
           {errors.previewImage && (
             <div color="red" className="errors">
               {errors.previewImage}
             </div>
           )}
-          <input
-            className="images"
-            name="image1"
-            type="text"
-            value={image1}
-            placeholder="Image URL"
-            onChange={(e) => setImage1(e.target.value)}
-          />
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          {textInput(image1, "Image URL", setImage1)}
           {errors.image1 && (
             <div color="red" className="errors">
               {errors.image1}
             </div>
           )}
-          <input
-            className="images"
-            name="image2"
-            type="text"
-            value={image2}
-            placeholder="Image URL"
-            onChange={(e) => setImage2(e.target.value)}
-          />
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          {textInput(image2, "Image URL", setImage2)}
           {errors.image2 && (
             <div color="red" className="errors">
               {errors.image2}
             </div>
           )}
-          <input
-            className="images"
-            name="image3"
-            type="text"
-            value={image3}
-            placeholder="Image URL"
-            onChange={(e) => setImage3(e.target.value)}
-          />
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          {textInput(image3, "Image URL", setImage3)}
           {errors.image3 && (
             <div color="red" className="errors">
               {errors.image3}
             </div>
           )}
-          <input
-            className="images"
-            name="image4"
-            type="text"
-            value={image4}
-            placeholder="Image URL"
-            onChange={(e) => setImage4(e.target.value)}
-          />
+          {/*
+          //!-------------------------------------------------------------------
+          */}
+          {textInput(image4, "Image URL", setImage4)}
           {errors.image4 && (
             <div color="red" className="errors">
               {errors.image4}
             </div>
           )}
         </div>
+        {/*
+        //!-------------------------------------------------------------------
+        //?----------------------- SUBMIT BUTTON -----------------------------
+        //!-------------------------------------------------------------------
+        */}
         <button type="submit">Create Spot</button>
       </form>
     </div>
